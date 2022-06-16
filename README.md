eb495377a4a7edb7bcfec58727746e423e4c3365
Ashley's PC <ashlynn4567@gmail.com>
2022-06-16 16:25:38 -0500

    updated seeds to reflect schema

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Employee-Tracker
-This is my twelfth challenge assignment for the University of Oregon Coding Bootcamp 2022. In this challenge, I use Node.js, Inquirer, and SQL to create a database connects with the command line interface. Users can search for employee information by entering key words into each inquiry prompt. 
+
+This is my twelfth challenge assignment for the University of Oregon Coding Bootcamp 2022. In this challenge, I use Node.js, Inquirer, and SQL to create a database connects with the command line interface. Users can search for employee information by entering key words into each inquiry prompt.
+
+include installation instructions with .env file and updating variables to include the user's username and password
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,43 +1,42 @@
-INSERT INTO departments 
+INSERT INTO department
     (name)
 VALUES 
-    ("Software Development"),
-    ("Front Office"),
-    ("Food and Beverage"),
-    ("Housekeeping"),
-    ("Sales"),
-    ("Human Resources");
+    ("Product Management"),
+    ("Front-End Development"),
+    ("Back-End Development"),
+    ("Full-Stack Development");
 
-INSERT INTO roles
+INSERT INTO role
     (title, salary, department_id)
 VALUES 
-    ("Front-End Developer", 60000, 1),
-    ("Back-End Developer", 60000, 1),
-    ("Full-Stack Development Coordinator", 75000, 1),
-    ("Director of Rooms", 50000, 2),
-    ("Front Office Manager", 40000, 2),
-    ("Front Desk Associate", 30000, 2),
-    ("Director of Food and Beverage", 50000, 3),
-    ("Restaurant Manager", 40000, 3),
-    ("Cook", 35000, 3),
-    ("Server", 30000, 3),
-    ("Director of Style", 50000, 4),
-    ("Housekeeping Supervisor", 40000, 4),
-    ("Housekeeper", 30000, 4),
-    ("Director of Sales", 50000, 5), 
-    ("Assistant Director of Sales", 40000, 5),
-    ("Sales Associate", 30000, 5),
-    ("Director of Human Resources", 50000, 6), 
-    ("Human Resources Associate", 35000, 6);
+    ("Product Owner", 130000, 1),
+    ("Project Manager", 120000, 1),
+    ("Team Lead", 110000, 1),
+    ("Tech Lead", 110000, 1),
+    ("QA Lead", 110000, 1),
+    ("QA Engineer", 110000, 1),
+    ("UI Designer", 90000, 2),
+    ("Front-End Developer", 80000, 2),
+    ("UX Designer", 90000, 3),
+    ("Back-End Developer", 80000, 3),
+    ("Full-Stack Developer", 100000, 4),
+    ("Database Specialist", 80000, 4),
+    ("Tester", 80000, 4);
 
-INSERT INTO employees
-    (first_name, last_name, role_id, employee_id)
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
 VALUES 
-    ("Ashley", "Smith", 1, NULL),
-    ("Shayne", "Georgeoff", 2, 1),
-    ("Jenna", "Palmer", 3, 2),
-    ("Jasmon", "Martin", 4, NULL),
-    ("Ky", "Rudolph", 5, 1),
-    ("Annie", "Robinson", 6, 5), 
-    ("Joseph", "Williams", 6, 5),
-    ("Elizabeth", "Klenke", 6, 5);
+    ("Taliah", "Daniels", 1, NULL),
+    ("Aeryn", "Santos", 1, 1),
+    ("Bo", "Henry", 1, 2),
+    ("Casper", "Cisneros", 1, 2),
+    ("Sarah", "Kirkpatrick", 1, 2),
+    ("Emir", "Huber", 1, 2),
+    ("Kaelan", "Felix", 2, 3),
+    ("Darian", "Hunter", 2, 3),
+    ("Vernon", "Walls", 3, 4),
+    ("Mary", "Winter", 3, 4),
+    ("Iman", "Barnes", 4, 3),
+    ("Erica", "Sinclair", 4, 4),
+    ("Tanvir", "Gibbs", 4, 4);
+
